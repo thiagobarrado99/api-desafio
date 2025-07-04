c239f3cbabff553d0c84dc0278097576022cb908
Thiago <thiagobarrado99@gmail.com>
2025-07-04 02:39:11 -0300

    API routing

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\BillController;
+use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\RevenueController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('log.requests')->group(function () {
+    Route::resource("customers", CustomerController::class, [
+        'only' => ['index', 'store']
+    ]);
+
+    Route::get('customers/{id}/bills', [CustomerController::class, 'customerBills']);
+
+    Route::resource("bills", BillController::class, [
+        'only' => ['store']
+    ]);
+});

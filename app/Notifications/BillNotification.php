797b18f9c7d30a3d02fec2f4f76b85af6ad27ada
Thiago <thiagobarrado99@gmail.com>
2025-07-04 02:43:10 -0300

    Notification for billings

---
+++ app/Notifications/BillNotification.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class BillNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(protected array $billData)
+    {
+    }
+
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Cobrança gerada!')
+            ->greeting('Olá ' . $notifiable->name)
+            ->line('Cobrança #'.$this->billData['id'].' gerada com sucesso!')
+            ->line('Valor R$ ' . number_format($this->billData['amount'], 2, ",", "."))
+            ->salutation('At.te, Thiago Souza.');
+    }
+}
